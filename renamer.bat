77db08dde304cebe66e6952573d3b506973d6fae
Pasan Bhanu Guruge <pasanbguruge@hotmail.com>
2019-02-23 22:22:38 +0530

    Customize

--- renamer.bat
+++ renamer.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enableDelayedExpansion
-for %%F in (*-min*) do (
+for %%F in (*_COMMON_TEXT_*) do (
   set "name=%%F"
-  ren "!name!" "!name:-min=!"
+  ren "!name!" "!name:_COMMON_TEXT_=!"
 )
